1c4ab730667d379add01bc751746ab61913a7ed1
Rafi718 <rafiprovider4@gmail.com>
2024-08-22 01:33:45 +0700

    feat: spaite roles

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Inertia\Inertia;
 |
 */
 
+// Route::get('admin',function () {
+//     return 'Hi Admin';
+// })->middleware('role:admin');
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
